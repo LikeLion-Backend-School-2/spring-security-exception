3798968c0265aca73dc4dd005f7ca0c705d915d5
seoyun <may-primavera@naver.com>
2022-12-05 15:34:14 +0900

    feat: review controller + username 꺼내기

--- src/main/java/springboot/springsecurity/controller/ReviewController.java
+++ src/main/java/springboot/springsecurity/controller/ReviewController.java
@@ -1,17 +1,21 @@
 package springboot.springsecurity.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springboot.springsecurity.domain.dto.ReviewCreateRequest;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/reviews")
 public class ReviewController {
 
     @PostMapping("")
-    public String write(@RequestBody ReviewCreateRequest reviewCreateRequest) {
+    public String write(@RequestBody ReviewCreateRequest reviewCreateRequest, Authentication authentication) {
+        log.info("{}", authentication.getName());
         return "리뷰 등록에 성공했습니다";
     }
 
