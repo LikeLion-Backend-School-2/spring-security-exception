83ea40692a1b8749905cbd0f1db52d1a39c9559d
seoyun <may-primavera@naver.com>
2022-12-05 15:33:52 +0900

    feat: JwtUtil Claims

--- src/main/java/springboot/springsecurity/util/JwtUtil.java
+++ src/main/java/springboot/springsecurity/util/JwtUtil.java
@@ -7,6 +7,20 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import java.util.Date;
 
 public class JwtUtil {
+
+    private static Claims extractClaims(String token, String key) {//일종의 map 키:밸류로 되어있다
+        return Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody();
+    }
+
+    public static String getUserName(String token, String key) {
+        return extractClaims(token, key).get("username").toString();
+    }
+    public static boolean isExpired(String token, String secretKey) {
+        //expire timestamp를 return 함
+        Date expiration = extractClaims(token, secretKey).getExpiration();
+        return expiration.before(new Date());
+    }
+
     public static String createToken(String username, String key, long expireTile) {
         Claims claims = Jwts.claims();
         claims.put("username", username);
